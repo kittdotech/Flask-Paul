30a4fb7bcfaa2af23a985ee3d64e72674c1e1588
Rajiv <kittdotech@gmail.com>
2015-06-29 07:06:17 +0530

    With Secondary key rendering

--- Flask.py
+++ Flask.py
@@ -118,27 +118,27 @@ def index(name=None):
 
 @app.route('/search',methods = ['POST','GET'])
 def search(key=None):
-    path=os.path.dirname(os.path.abspath(__file__))+'/json'
+    path=os.path.dirname(os.path.abspath(__file__))
     json_list={}
-    for root, dirs, files in os.walk(os.path.dirname(path)):
+    for root, dirs, files in os.walk(os.path.join(path,'json')):
         for f in files:
            if f.lower().endswith((".json")):
               try:
                 count=1
-                with open(root+'/'+f) as data_file:
+                with open(os.path.join(root,f)) as data_file:
                         for data in json_parse(data_file):
-                            title=data['article_title']
+                            title=data['wiki']['article_title']
                             articles=[]
                             collection={}
                             found=False
-                            title=data['article_title'];
-                            for a in data['article_sections']:
+
+                            for a in data['wiki']['article_sections']:
 
                                     str_section=re.sub(r'\n[\n]*', r'<br/><br/>', a['section_text'])
                                     section_number=a['section_number']
                                     str_section_name=a['section_name']
                                     str_find=request.args['sk']
-                                    print section_number
+
                                     articles.insert(section_number,{str_section_name:str_section})
                                     res=string.find(str_section.lower() ,str_find.lower())
 
@@ -148,13 +148,27 @@ def search(key=None):
 
                                         if not collection:
                                             Summary=data.get('Summary', " ")
-                                            Links=walk(data.get('Links', ""))
-                                            Hierarchy=walk(data.get('Hierarchy   ', ""))
+
+                                            Links="<table cellspacing='10'>"
+                                            for k in data['triples']['link_subs']:
+                                                Links+=("<tr><td>"+k[0]+"</td><td>"+k[1]+"</td><td>"+k[2]+"</td></tr>")
+                                            Links+="</table>"
+
+                                            Hierarchy=""
+                                            for k in data['wm']['textReps']['reps']:
+                                                if not k['children']:
+                                                    Hierarchy+=('<p class="hierarchy-all" style="padding-left:' + str(k['level']*3)+'em;">'+k['sentence']+'</p><br/><br/>')
+                                                else:
+                                                    Hierarchy+=('<p class="hierarchy-all hierarchy-child" style="padding-left:'+ str(k['level']*3)+'em;">'+k['sentence']+'</p><br/><br/>')
+
+
                                             Inferences=walk(data.get('Inferences', ""))
                                             Reps=walk(data.get('Reps', ""))
                                             Stats=walk(data.get('Stats', ""))
                                             collection.update({'Summary':Summary})
                                             collection.update({'Links':Links})
+
+
                                             collection.update({'Hierarchy':Hierarchy})
                                             collection.update({'Inferences':Inferences})
                                             collection.update({'Reps':Reps})
@@ -169,7 +183,7 @@ def search(key=None):
                 print e.message
     if not json_list:
         json_list.update({'error':'NO result found...'})
-    print json_list
+    #print json_list
     return jsonify(json_list)
     #return 'hello'
 
