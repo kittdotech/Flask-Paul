4aa4520ee7da0c2187acf8d917adfa4d90f9de8f
Rajiv <kittdotech@gmail.com>
2015-07-01 19:56:24 +0530

    Optimized Code for Speed.

--- Flask.py
+++ Flask.py
@@ -1,6 +1,8 @@
 from flask import Flask, session
 from flask import render_template
-import os,json
+import os
+#import json
+import ujson as json
 from json import JSONDecoder
 from flask import jsonify
 from flask import request
@@ -18,8 +20,6 @@ string_types = (str, unicode) if str is bytes else (str, bytes)
 iteritems = lambda mapping: getattr(mapping, 'iteritems', mapping.items)()
 
 
-
-import types
 
 def walk(node):
 	str=""
@@ -116,6 +116,16 @@ def index(name=None):
     return render_template('index.html',name=name,page=1,sk=sk)
     #return 'hello'
 
+
+
+@app.route('/searchjson',methods = ['POST','GET'])
+def searchjson():
+    data_file_path=request.args['jsonfile']
+    with open(data_file_path) as data_file:
+        data=json.load(data_file)
+    return jsonify(data)
+
+
 @app.route('/search',methods = ['POST','GET'])
 def search(key=None):
     path=os.path.dirname(os.path.abspath(__file__))
@@ -124,14 +134,12 @@ def search(key=None):
         for f in files:
            if f.lower().endswith((".json")):
               try:
-                count=1
-                with open(os.path.join(root,f)) as data_file:
-                        for data in json_parse(data_file):
-                            title=data['wiki']['article_title']
-                            articles=[]
-                            collection={}
-                            found=False
 
+                str_path=os.path.join(root,f)
+                with open(os.path.join(root,f)) as data_file:
+                            data=json.load(data_file)
+                            title=data['wiki']['article_title']
+                            found=False
                             for a in data['wiki']['article_sections']:
 
                                     str_section=re.sub(r'\n[\n]*', r'<br/><br/>', a['section_text'])
@@ -139,52 +147,22 @@ def search(key=None):
                                     str_section_name=a['section_name']
                                     str_find=request.args['sk']
 
-                                    articles.insert(section_number,{str_section_name:str_section})
+                                    #articles.insert(section_number,{str_section_name:str_section})
                                     res=string.find(str_section.lower() ,str_find.lower())
 
                                     if res!=-1:
                                         found=True
-
-
-                                        if not collection:
-                                            Summary=data.get('Summary', " ")
-
-                                            Links="<table cellspacing='10'>"
-                                            for k in data['triples']['link_subs']:
-                                                Links+=("<tr><td>"+k[0]+"</td><td>"+k[1]+"</td><td>"+k[2]+"</td></tr>")
-                                            Links+="</table>"
-
-                                            Hierarchy=""
-                                            for k in data['wm']['textReps']['reps']:
-                                                level=k['level']
-                                                if not k['children']:
-                                                    Hierarchy+=('<p class="hierarchy-all" style="padding-left:' + str(level*50)+'px;">'+k['sentence']+'</p><br/><br/>')
-                                                else:
-                                                    Hierarchy+=('<p class="hierarchy-all hierarchy-child" style="padding-left:'+ str(level*50)+'px;">'+k['sentence']+'</p><br/><br/>')
-                                            Inferences=walk(data.get('Inferences', ""))
-                                            Reps=walk(data.get('Reps', ""))
-                                            Stats=walk(data.get('Stats', ""))
-                                            collection.update({'Summary':Summary})
-                                            collection.update({'Links':Links})
-
-
-                                            collection.update({'Hierarchy':Hierarchy})
-                                            collection.update({'Inferences':Inferences})
-                                            collection.update({'Reps':Reps})
-                                            collection.update({'Stats':Stats})
+                                        break
 
                             if found:
-                                json_list.update({title:{'articles_details':articles,'collection_details':collection}})
-
-
+                                json_list.update({title:{'path':str_path}})
 
               except Exception as e:
                 print e.message
     if not json_list:
         json_list.update({'error':'NO result found...'})
-    #print json_list
     return jsonify(json_list)
-    #return 'hello'
+
 
 
 def json_parse(fileobj, decoder=JSONDecoder(), buffersize=2048):
